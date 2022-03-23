962a986ddeaf33dc8234aac9f0a8bda575f7b3b8
tpawlen <t.pawlen@gmail.com>
2022-03-23 15:51:42 -0600

    updated code

--- leachandrew_nrgstream_graphs.R
+++ leachandrew_nrgstream_graphs.R
@@ -1,5 +1,5 @@
 library(png)
-
+#library(gghighlight)
 
 load("nrgstream_gen.RData") ## which is here *equivalent* to
 
@@ -50,7 +50,7 @@ df2 <- df1 %>%
   group_by(Plant_Type,Year) %>% 
   summarise(capture = sum(total_rev)/sum(total_gen),
             avg_rev = sum(total_rev)/sum(total_gen),
-            p_mean=mean(p_mean))
+            p_mean=mean(p_mean, na.rm = TRUE))
 
 df2$Plant_Type<-fct_relevel(df2$Plant_Type, "OTHER",after=Inf)
 df2$Plant_Type<-fct_relevel(df2$Plant_Type, "HYDRO",after=Inf)
@@ -82,18 +82,30 @@ my_palette<-c(colors_tableau10()[8],
               colors_tableau10()[9],
               colors_tableau10_light()[9])
 
-ggplot(df2,aes(Year,capture-p_mean,colour=Plant_Type,fill=Plant_Type),alpha=0.5)+
+df6 <- df2 %>%
+  #  filter(Plant_Type != "MARKET") %>%
+  filter(as.character(Year) >= 2014)
+
+my_palette1<-c("grey81","coral4","goldenrod4","tan","slategray1",
+              "lightskyblue","green4","lightgoldenrod2","thistle3","rosybrown")
+
+plot_b <- ggplot(df6,
+                 aes(Year,capture-p_mean,colour=Plant_Type,fill=Plant_Type),
+                 alpha=0.5)+
   geom_col(aes(Year,capture-p_mean,colour=Plant_Type,fill=Plant_Type),
            size=1.5,position = position_dodge(width = .9),width = .6)+
-  #scale_color_viridis("Plant Type",discrete=TRUE)+
-  #scale_fill_viridis("Plant Type",discrete=TRUE)+
-  scale_color_manual("",values=my_palette)+
-  scale_fill_manual("",values=my_palette)+
+  scale_color_manual("",values=my_palette1)+
+  scale_fill_manual("",values=my_palette1)+
   
   slide_theme()+
-  labs(x="",y="Revenue Relative to Mean Price ($/MWh)",
-       title="Energy Price Capture Differential ($/MWh, 2010-2020)",
-       caption="Source: AESO Data, accessed via NRGStream\nGraph by @andrew_leach")
+  labs(x="",y="Revenue Relative to \nMean Price ($/MWh)")+#,
+#       title="Energy Price Capture Differential ($/MWh, 2014-2021)")+#,
+#       caption="Source: AESO Data, accessed via NRGStream\nGraph by @andrew_leach") +
+  theme(panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        legend.background = element_rect(fill = "transparent"),
+        legend.box.background = element_rect(fill = "transparent", color = "transparent")) 
+
 
 df3 <- df1 %>% filter(Plant_Type %in% gen_set,year(time)<2022) %>%
   group_by(Year) %>% summarise(capture = sum(total_rev)/sum(total_gen),avg_rev = sum(total_rev)/sum(total_gen),p_mean=mean(p_mean))%>%
@@ -107,36 +119,55 @@ df4<-tibble(Year=seq(2010,2016),Plant_Type="SOLAR",capture=0)%>%mutate(Year=as_f
                                                                        Plant_Type=as_factor(Plant_Type))
 df2<-df2 %>% bind_rows(df4)
 
+df5 <- df2 %>%
+#  filter(Plant_Type != "MARKET") %>%
+  filter(as.character(Year) >= 2014)
+
+mark <- df2 %>%
+  filter(Plant_Type == "MARKET") %>%
+  filter(as.character(Year) >= 2014)
 
 #my_palette<-c("black",colors_tableau10()[8],colors_tableau10_medium()[4],colors_tableau10()[4],colors_tableau10_light()[4],colors_tableau10()[7],colors_tableau10()[1],colors_tableau10()[3],colors_tableau10()[2],colors_tableau10()[9],colors_tableau10_light()[9])
-my_palette<-c("black","grey81","coral4","goldenrod4","tan","slategray1",
-              "dodgerblue","forestgreen","gold","indianred1","darkkhaki")
+my_palette<-c("black", "grey81","coral4","goldenrod4","tan","slategray1",
+              "lightskyblue","green4","lightgoldenrod2","thistle3","rosybrown")
 #my_palette<-c("black",grey.colors(10,start=0.2,end = .95))
 
-plot_a <- ggplot(filter(df2,as.character(Year)>=2014),
+plot_a <- ggplot(df5,
                aes(Year,capture,colour=Plant_Type,fill=Plant_Type),alpha=0.5)+
   geom_col(aes(Year,capture,colour=Plant_Type,fill=Plant_Type),
-           size=0.8,position = position_dodge(width = .9),width = .6, alpha=0.9)+
-  #  scale_color_viridis("Plant Type",discrete=TRUE)+
-  #  scale_fill_viridis("Plant Type",discrete=TRUE)+
-  #scale_color_manual("",values=colors_tableau10())+
-  #scale_fill_manual("",values=colors_tableau10())+
+           size=1,position = position_dodge(width = .9),width = .6)+
+  geom_line(data= mark, group=1, aes(Year,capture)) +
   scale_color_manual("",values=my_palette)+
   scale_fill_manual("",values=my_palette)+
   slide_theme()+
-  labs(x="Year",y="Average Revenue ($/MWh)",
-       title="Energy Price Capture ($/MWh, 2014-2021)")+
+  labs(x="",y="Average Revenue \n($/MWh)")+#,
+#       title="Energy Price Capture ($/MWh, 2014-2021)")+#,
+  #     caption="Source: AESO Data, accessed via NRGStream\nGraph by @andrew_leach") +
   theme(panel.background = element_rect(fill = "transparent"),
-#        panel.grid.major = element_blank(),
-#        panel.grid.minor = element_blank(),
         plot.background = element_rect(fill = "transparent", color = NA),
-#        axis.title = element_blank(),
-#        axis.text = element_blank(),
-#        axis.ticks = element_blank(),
+        legend.position = "right",
         legend.background = element_rect(fill = "transparent"),
-#        legend.box = NULL,
         legend.box.background = element_rect(fill = "transparent", color = "transparent")) 
 
 plot_a
 
-ggsave(path = "images", filename = "windfarmlocations.png", bg = "transparent")
+g_legend<-function(a.gplot){
+  tmp <- ggplot_gtable(ggplot_build(a.gplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)}
+
+cp_plot2 <- ggarrange(ggarrange(plot_a + theme(legend.position = "none"),
+                                  NULL,
+                                 plot_b + theme(legend.position = "none"),
+                                 nrow=3, heights = c(1,-0.2,1)),
+                     g_legend(plot_a), 
+                     ncol=2, widths=c(6,1))
+
+cp_plot <- annotate_figure(cp_plot2, 
+                           fig.lab = "Source: AESO Data, accessed via NRGStream\nGraph by @andrew_leach",
+                           fig.lab.pos = "bottom.right", 
+                           fig.lab.face = "italic", 
+                           fig.lab.size = 10)
+
+ggsave(path = "images", filename = "captureprice.png", bg = "transparent")
