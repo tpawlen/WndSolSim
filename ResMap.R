563bcc267c959563bda20f4f23dd63be6ead085a
tpawlen <t.pawlen@gmail.com>
2022-03-24 10:34:27 -0600

    Cleaned up map

--- ResMap.R
+++ ResMap.R
@@ -38,35 +38,46 @@
 setwd("D:/Documents/GitHub/AuroraEval")
 getwd()
 
+
+# Load in the data
+# Wind Speed data from Canada Wind Atlas 
+# http://www.windatlas.ca/nav-en.php?no=46&field=EU&height=80&season=ANU
 wind_profile <- readRDS("WindAtlas_Data_0.05")
 colnames(wind_profile) <- c('Latitude', 'Longitude', 'Wind')
 
+# Location of operational wind turbines, from Canadian Wind Turbine Database
+# https://open.canada.ca/data/en/dataset/79fdad93-9025-49ad-ba16-c26d718cc070
 turb_location <- read_excel("Wind_Turbine_Database_FGP.xlsx")
 turb_AB <- turb_location %>%
   filter(Province=="Alberta")
 
+# Location of wind farms in AESO queue. Long and Lat were determined manually 
+# using Google Maps
+# https://www.aeso.ca/grid/projects/connection-project-reporting/
 res_pot <- read_excel("AESO_Connection_List.xlsx")
 turb_pot <- res_pot %>%
   filter(Technology=="Wind")
+turb_pot$Status <- "Planning"
+turb_pot <- subset(turb_pot, select = -Technology)
 
-# Summarizes the wind farms.
+# Summarizes the active wind farms.
 plant <- turb_AB %>%
   group_by(`Project name`, `Total project capacity (MW)`) %>%
   summarise(Latitude = mean(Latitude), Longitude = mean(Longitude)) %>%
   rename(Capacity = `Total project capacity (MW)`)
+plant$Status <- "Active"
+
+# Combine the active and planned wind farms
+wind_farm <- rbind(plant,turb_pot)
 
 #Level 1 shows provinces, while level 2 shows individual counties
 can_level1 = getData("GADM", country = "CA", level = 1)
-#can_level2 = getData("GADM", country = "CA", level =2)
 
 WGS84 <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
 canada_level1_ellipsoid = spTransform(can_level1, WGS84)
-#canada_level2_ellipsoid = spTransform(can_level2, WGS84)
 
 alberta_ellipsoid1 = 
   canada_level1_ellipsoid[which(canada_level1_ellipsoid$NAME_1 == "Alberta"),]
-#alberta_ellipsoid = 
-#  canada_level2_ellipsoid[which(canada_level2_ellipsoid$NAME_1 == "Alberta"),]
 
 # Excludes any points outside the province
 #inout = over(
@@ -75,31 +86,14 @@ alberta_ellipsoid1 =
 #)
 
 # Map of Alberta
-ggplot() + 
-  geom_tile(data = wind_profile,#[!is.na(inout),], 
+AB <- ggplot() + 
+  geom_tile(data = wind_profile, 
             aes(x = Longitude, y = Latitude, fill = Wind)) +
   geom_polygon(data = alberta_ellipsoid1, 
                aes(x = long, y = lat, group = group), 
                fill = "transparent", colour = "black") +
   scale_fill_gradientn(colors = matlab.like2(100),
-                       limits=c(3,10),oob=squish) +
-#  geom_point(data = turb_pot, 
-#             aes(x = Longitude, y = Latitude, size = Capacity, shape = 18), 
-#             color = "orangered") +
-#  geom_point(data = plant, 
-#             aes(x = Longitude, y = Latitude, size = Capacity, shape = 16),
-#             color = "limegreen") + 
-#  scale_y_continuous(limits = c(49, 60.1)) +
-#  scale_color_continuous(name = "Average Wind Speed at 80m",
-#                         guide = legend)
-#  scale_shape_identity(name = "Status",
-#                       breaks = c(18, 16),
-#                       labels = c("AESO Planning", "Active"),
-#                       guide = "legend") +
-#  scale_color_identity(name = "Status",
-#                       breaks = c("orangered", "limegreen"),
-#                       labels = c("AESO Planning", "Active"),
-#                       guide = "legend") +
+                       limits=c(3,10),oob=squish, name = "Mean Wind Speed \nat 80m height \n(m/s)") +
   theme(panel.background = element_rect(fill = "transparent"),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
@@ -109,6 +103,13 @@ ggplot() +
         axis.ticks = element_blank(),
         legend.background = element_rect(fill = "transparent"),
         legend.box.background = element_rect(fill = "transparent", color = "transparent")) 
+
+AB + geom_point(data = wind_farm,
+                aes(x= Longitude, y = Latitude, size = Capacity, shape = Status, color = Status)) + 
+  scale_shape_manual(values = c(16,18), labels = c("Active","AESO Planning")) +
+  scale_color_manual(values = c("black", "grey39"), 
+                     labels = c("Active","AESO Planning")) +
+  guides(shape = guide_legend(override.aes = list(size = 5)))
 
 ggsave(path = "images", filename = "windfarmlocations.png", bg = "transparent")
 
