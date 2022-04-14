5604603868802fc58c593cfecaf42b28a3c30387
tpawlen <t.pawlen@gmail.com>
2022-04-14 11:44:35 -0600

    Another run

--- WindPotential.R
+++ WindPotential.R
@@ -29,8 +29,8 @@ colnames(wind_pot) <- x
 {
   maxLat <- 60
   maxLon <- -120
-  minLat <- 49.01
-  minLon <- -110.01
+  minLat <- 49.04
+  minLon <- -110.04
   
   totLat <- maxLat-minLat
   totLon <- abs(maxLon-minLon)
@@ -82,7 +82,7 @@ row.names(wind_prof) <- 1:nrow(wind_prof)
   # Note the start time
   old <- Sys.time()
   {  
-  for(i in 1:nrow(wind_prof)) {
+  for(i in 24444:nrow(wind_prof)) {
     tempold <- Sys.time()
     url <- paste(cons1, wind_prof[i,1], cons2, wind_prof[i,2], sep = "")
     wind <- readHTMLTable(url,which=1)
@@ -97,8 +97,9 @@ row.names(wind_prof) <- 1:nrow(wind_prof)
     #print(paste(wind_prof[i,1],wind_prof[i,2],sep = ","))
   }
 
-     # Creates an RDS file with the entire dataset.
-  saveRDS(wind_prof, file = paste("WindAtlas_Data1", res, sep = "_"))
+     
+   # Creates an RDS file with the entire dataset.
+  saveRDS(wind_prof, file = paste("WindAtlas_Data44", res, sep = "_"))
   
   # Prints the location of the file.
   getwd()
@@ -107,4 +108,4 @@ row.names(wind_prof) <- 1:nrow(wind_prof)
   New <- Sys.time() - old
   print(New)
 }
-                            
+                                      
